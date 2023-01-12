4a96b62f90e576a770f0daf5903511c499cb86f0
bbaiser <benj.baiser@gmail.com>
2023-01-12 11:48:34 -0500

    call in bee data

---
+++ Code/clean_data.R
@@ -0,0 +1,9 @@
+###Sandhill bee data cleaning and prep###
+
+#Data
+#bee data with Lasioglossum as sp. for bowl traps (bt) and vane traps (VT)
+bee_sp<-read.csv("data/dataBee.csv", header = T, row=1)#
+
+#Lasioglossum species designations
+lassio_sp<-read.csv("data/Lassioglossum_species.csv", header=T)
+H
