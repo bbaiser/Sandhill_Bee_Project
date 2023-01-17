efdcb5d28da1fd95aeb9ea88f238794bf77cc521
bbaiser <benj.baiser@gmail.com>
2023-01-17 14:29:48 -0500

    Drop vt and bt lassiogossum

--- Code/clean_data.R
+++ Code/clean_data.R
@@ -1,4 +1,6 @@
 ###Sandhill bee data cleaning and prep###
+library(dplyr)
+library(tidyverse)
 
 #Data
 #bee data with Lasioglossum as sp. for bowl traps (bt) and vane traps (VT)
@@ -6,4 +8,9 @@ bee_sp<-read.csv("data/dataBee.csv", header = T, row=1)#
 
 #Lasioglossum species designations
 lassio_sp<-read.csv("data/Lassioglossum_species.csv", header=T)
-H
+
+
+bee_drop<-bee_sp %>%
+       filter(!(Trap_Type=="NT"& Genus== "Lasioglossum"))
+df %>% 
+  filter(!(parameter=="param_A" & year == 2003))
