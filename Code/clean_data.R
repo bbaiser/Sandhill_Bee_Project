373bf2713056d6d00c440db11d712e1b28abd9c3
bbaiser <benj.baiser@gmail.com>
2023-01-26 14:47:46 -0500

    replcated multiples and matched las _ind to bee_drop

--- Code/clean_data.R
+++ Code/clean_data.R
@@ -15,19 +15,31 @@ las_scott<-read.csv("data/Lasioglossums_5202020.csv", header=T)
 
 #Drop Lasioglossum sp. from vane trap and bowl trap
 bee_drop<-bee_sp %>%
-          filter(!(Genus== "Lasioglossum" & Trap_Type=="BT"|Genus=="Lasioglossum" & Trap_Type=="VT"))
+          filter(!(Genus== "Lasioglossum" & Trap_Type=="BT"|Genus=="Lasioglossum" & Trap_Type=="VT"))%>%
+          select(-Sex)
 
 
 #make a new month column from sample data
 las_scott_mon<-las_scott%>%
-    mutate(Mon=str_extract( Date,"[A-z]+"))%>%
-    mutate(Month=str_replace_all(dd$Month, c("Mar"="March","May"="May", "Apr"="April", "Jul"="July", "Jun"="June", "Sep"="September",  "Aug"= "August", "Oct"="October" ,"Nov"="November")))%>%
-    select(-Mon)
+               mutate(Mon=str_extract( Date,"[A-z]+"))%>%
+               mutate(Month=str_replace_all(dd$Month, c("Mar"="March","May"="May", "Apr"="April", "Jul"="July", "Jun"="June", "Sep"="September",  "Aug"= "August", "Oct"="October" ,"Nov"="November")))%>%
+               select(-Mon)
 
 #take all singletons out
 las_sing<-las_scott_mon%>%
           filter(extra.count==0)
 
-#deal with multiples
-JobDF %>% 
-  mutate(inNameDF = ifelse(str_detect(occupation, paste0(NameDF$names, collapse = "|")),"yes","no"))
+#make replicates for multiples and bind with singletons
+las_ind<-las_scott_mon%>%
+         filter(!extra.count==0)%>% 
+         uncount(extra.count)%>%
+         bind_rows(las_sing)%>%
+         rename (Trap_Type=Trap,Genus=Genera,Species=species,Plotnum=Plot)%>%
+         select(-Date,-sex..ref.bee.only.,-Family,-extra.count,-Order)%>%
+         mutate(Plot= paste(Site, Plotnum, sep = ''))%>%
+         select(-Plotnum)%>%
+         mutate(Plot_Month = paste(Plot, Month, sep = '_'))%>%
+         mutate(Species2 = paste(Genus, Species, sep = ' '))%>%
+         select(-Species)%>%
+         rename(Species=Species2)
+
